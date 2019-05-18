361bad163ca86a26d4aed1ccda6262957add1d80
lobstex <39928129+lobstex@users.noreply.github.com>
2019-05-18 13:14:19 +0530

    Update Lobstex-setup.sh

--- Lobstex-setup.sh
+++ Lobstex-setup.sh
@@ -282,7 +282,7 @@ then edit the lobstex.conf file and save it in nano: (Ctrl-X + Y + Enter),
 then start the lobstexd daemon back up:
 to stop:   ${YELLOW}./lobstex-cli stop${NC}
 to edit:   ${YELLOW}nano ~/.lobstex/lobstex.conf${NC}
-to start:  ${YELLOW}./lobstexd${NC}"
+to start:  ${YELLOW}./lobstexd${NC}
 ========================================================================
 To view lobstexd debug log showing all MN network activity in realtime:
 ${YELLOW}tail -f ~/.lobstex/debug.log${NC}
@@ -294,25 +294,5 @@ Enjoy your Lobstex Masternode and thanks for using this setup script!
 
 clear_stdin
 #
-
-function setup_node() {
-  get_ip
-  create_config
-  create_key
-  update_config
-  enable_firewall
-  important_information
-  configure_systemd
-}
-
-
-##### Main #####
-clear
-
-purgeOldInstallation
-checks
-prepare_system
-download_node
-setup_node
 
 EOF
